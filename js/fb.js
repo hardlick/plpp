dff8c2203533e7fcaf272c5dfaaed2b40f905a91
Hugo Casanova <hugo.casanova@ibusplus.com>
2019-06-27 07:49:47 -0500

    Stars

--- js/fb.js
+++ js/fb.js
@@ -1,83 +1,91 @@
-$(document).ready(function(){   
- 
- // add event listener on the login button
- 
- $("#login").click(function(){
-
-    facebookLogin();
-
-   
- });
-
- // add event listener on the logout button
-
- $("#logout").click(function(){
-
-   $("#logout").hide();
-   $("#login").show();
-   $("#status").empty();
-   facebookLogout();
-
- });
-
-   $(document).on('click', '#procesarReview', function () {
-                        
-                    });
-
- function facebookLogin()
- {
-   FB.getLoginStatus(function(response) {
-       console.log(response);
-       statusChangeCallback(response);
-   });
- }
-
- function statusChangeCallback(response)
- {
-     console.log(response);
-     if(response.status === "connected")
-     {
-        $("#login").hide();
-        $("#logout").show(); 
-        
-        fetchUserProfile();
-        $("#formReview").show(); 
-     }
-     else{
-         // Logging the user to Facebook by a Dialog Window
-         facebookLoginByDialog();
-     }
- }
-
- function fetchUserProfile()
- {
-   console.log('Welcome!  Fetching your information.... ');
-   FB.api('/me?fields=id,name,email,gender,birthday', function(response) {
-     console.log(response);
-     console.log('Successful login for: ' + response.name);
-     $('#name').val(response.name);
-     $('#email').val(response.email);
+$(document).ready(function () {
+  $('.kv-ltr-theme-fas-star').rating({
+        hoverOnClear: false,
+        theme: 'krajee-fas',
+        containerClass: 'is-star',
+        'language' : 'es'
+    });
     
-   });
- }
+    $('.kv-rtl-theme-fas-alt').rating({
+        hoverOnClear: false,
+        theme: 'krajee-fas',       
+          'language' : 'es'
+    });
+    
+    $("#login").click(function () {
+        facebookLogin();
+    });
 
- function facebookLoginByDialog()
- {
-   FB.login(function(response) {
-      
-       statusChangeCallback(response);
-      
-   }, {scope: 'public_profile,email'});
- }
+    // add event listener on the logout button
 
- // logging out the user from Facebook
+    $("#logout").click(function () {
 
- function facebookLogout()
- {
-   FB.logout(function(response) {
-       statusChangeCallback(response);
-   });
- }
+        $("#logout").hide();
+        $("#login").show();
+        $("#status").empty();
+        facebookLogout();
+
+    });
+
+    $(document).on('click', '#procesarReview', function () {
+
+    });
+    function onload(){
+        
+    }
+    function facebookLogin()
+    {
+        FB.getLoginStatus(function (response) {
+            console.log(response);
+            statusChangeCallback(response);
+        });
+    }
+
+    function statusChangeCallback(response)
+    {
+        console.log(response);
+        if (response.status === "connected")
+        {
+            $("#login").hide();
+            $("#logout").show();
+
+            fetchUserProfile();
+            $("#formReview").show();
+        } else {
+            // Logging the user to Facebook by a Dialog Window
+            facebookLoginByDialog();
+        }
+    }
+
+    function fetchUserProfile()
+    {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me?fields=id,name,email,gender,birthday', function (response) {
+            console.log(response);
+            console.log('Successful login for: ' + response.name);
+            $('#name').val(response.name);
+            $('#email').val(response.email);
+
+        });
+    }
+
+    function facebookLoginByDialog()
+    {
+        FB.login(function (response) {
+
+            statusChangeCallback(response);
+
+        }, {scope: 'public_profile,email'});
+    }
+
+    // logging out the user from Facebook
+
+    function facebookLogout()
+    {
+        FB.logout(function (response) {
+            statusChangeCallback(response);
+        });
+    }
 
 
 });
