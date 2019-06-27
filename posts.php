583733643f092c7448d83af4f042e8ded3e0a1ba
Hugo Casanova <hugo.casanova@ibusplus.com>
2019-06-26 19:37:43 -0500

    Actualizando

--- posts.php
+++ posts.php
@@ -1,32 +1,17 @@
-<?php
-require_once __DIR__ . '/vendor/autoload.php'; // change path as needed
 
-$fb = new \Facebook\Facebook([
-  'app_id' => '1448141528671219',
-  'app_secret' => '89a44d740dd1d24f038edf629d0d1dd1',
-  'default_graph_version' => 'v3.3',
-  'default_access_token' => 'abaf7536a812fca085d340112c0fbd38', // optional
-]);
-
-/* PHP SDK v5.0.0 */
-/* make the API call */
-try {
-  // Returns a `Facebook\FacebookResponse` object
-  $response = $fb->get(
-    '/2924503311108158/visitor_posts',
-    'EAAUlE7lmoZCMBAKdtuKEbNGQfbBJTceSL6yxNfmBMSWW2MtYl28IZC3V0Q1aHDqbTBYBl7cNnYCzZBMlH26vAS0bADZA2w8mZB59kwxkRd9lTyENuBMjMCwZByadZBrJCrTrZA2ZCcZAUkgCCKfOE3iOv5Q5e88kO9tbPo0iprQXuHEA4dyAeOsfkC'
-  );
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-  echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-  echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  exit;
-}
-// If you have an old dev version of the official SDK
-$graphEdge = $response->getGraphList();
-// Or if you have the latest dev version of the official SDK
-$graphEdge = $response->getGraphEdge();
-var_dump($graphEdge);
-die();
-/* handle the result */
+<script type="text/javascript">
+    (function () {
+        var options = {
+            facebook: "518981848157046", // Facebook page ID
+            whatsapp: "+51943357937", // WhatsApp number
+            call_to_action: "Escríbenos", // Call to action
+            button_color: "#FF6550", // Color of button
+            position: "right", // Position may be 'right' or 'left'
+            order: "facebook,whatsapp" // Order of buttons
+        };
+        var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
