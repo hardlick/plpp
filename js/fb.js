aabc13e89759105e6db70dfd8d867e38c1c5813b
Hugo Casanova <hugo.casanova@ibusplus.com>
2019-06-26 23:13:26 -0500

    validando fb

--- js/fb.js
+++ js/fb.js
@@ -20,6 +20,9 @@ $(document).ready(function(){
 
  });
 
+   $(document).on('click', '#procesarReview', function () {
+                        
+                    });
 
  function facebookLogin()
  {
